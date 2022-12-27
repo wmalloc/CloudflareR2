a62a5fc2ca40907f3632b09489fcb5eb72792522
Waqar Malik <wmalloc@users.noreply.github.com>
2022-12-27 01:39:16 -0800

    Add Tests

--- Tests/R2Tests/R2Tests.swift
+++ Tests/R2Tests/R2Tests.swift
@@ -1,31 +1,14 @@
 import XCTest
 @testable import R2
 import XMLCoder
+import WebServiceURLMock
 
 final class R2Tests: XCTestCase {
     func testListBucketResult() throws {
-        let result = """
-        <?xml version= \"1.0\" encoding= \"UTF-8\"?>
-        <ListAllMyBucketsResult xmlns= \"http://s3.amazonaws.com/doc/2006-03-01/\">
-            <Buckets>
-                <Bucket>
-                    <CreationDate>2022-12-25T04:22:03.812Z</CreationDate>
-                    <Name>images</Name>
-                </Bucket>
-                <Bucket>
-                    <CreationDate>2022-12-25T04:23:03.352Z</CreationDate>
-                    <Name>videos</Name>
-                </Bucket>
-            </Buckets>
-            <Owner>
-                <DisplayName>asdfasdfasdfasdfa</DisplayName>
-                <ID>asdfasdfasdfasdfa</ID>
-            </Owner>
-        </ListAllMyBucketsResult>
-        """
+        let data = try Bundle.module.data(forResource: "ListAllMyBucketsResult", withExtension: "xml", subdirectory: "TestData")
         let decoder = XMLDecoder()
         decoder.shouldProcessNamespaces = true
-        let note = try decoder.decode(ListAllMyBucketsResult.self, from: result.data(using: .utf8)!)
-        print(note)
+        let buckets = try decoder.decode(ListAllMyBucketsResult.self, from: data)
+        print(buckets)
     }
 }
